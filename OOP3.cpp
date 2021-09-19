37128fe403a1da3e04e8833d25af9e912a43fe6e
Nikita_Kadatsky <cpencipogu@mail.ru>
2021-09-19 14:19:08 +0300

    COMPLITED WORK WELL

--- OOP3.cpp
+++ OOP3.cpp
@@ -1,28 +1,94 @@
-#include<bits/stdc++.h>
-#include "OPP3.h"
+//#include "stdafx.h"
+#include "OOP3.h"
+#include<iostream>
+void employee::display_all(){
+            cout<<sname<<" "<<name<<" "
+                <<patronymic<<" "<<date.day<<'.'<<(date.month <= 9 ? "0" : "" )
+                <<date.month<<'.'<<date.year<<" "<<division<<" "
+                <<position<<endl;
+}
+void employee::write_all(ofstream &FILE){
+            FILE<<sname<<" "<<name<<" "
+                <<patronymic<<" "<<date.day<<'.'<<(date.month <= 9 ? "0" : "" )
+                <<date.month<<'.'<<date.year<<" "<<division<<" "
+                <<position<<endl;
+}
+void employee::display_name_n_position_only(){
+    cout<<sname<<" "<<name<<" "<<patronymic<<" "<<position<<endl;
+}
+void employee::write__name_n_position_only(ofstream &FILE){
+    FILE<<sname<<" "<<name<<" "<<patronymic<<" "<<position<<endl;
+}
+void employee::read_from(ifstream &FILE){
+    FILE>>sname>>name>>patronymic>>date.day>>date.month>>date.year>>division>>position;
+}
+int employee::get_division(){
+        return division;
+}
+int employee::get_day(){
+    return date.day;
+}
+int employee::get_month(){
+    return date.month;
+}
+int employee::get_year(){
+    return date.year;
+}
+void fun1(employee *a, int n, ofstream &FILE){
+    int q;
+    cout<<"Enter division you require for\n";
+    cin>>q;
+    //ofstream outfile;
+    //outfile.open("outfile.txt", ios::app);
+    for(int i=0;i<n;i++){
+        if(a[i].get_division()==q){
+            a[i].display_name_n_position_only();
+            a[i].write__name_n_position_only(FILE);
+        }
+    }
+    //outfile<<"\n\n";
+    FILE.close();
+}
+void fun2(employee *a, int n, ofstream &FILE){
+    //ofstream outfile;
+    //outfile.open("outfile.txt", ios::app);
+    time_t now = time(0);
+    tm *ltm = localtime(&now);
+    //cout<<ltm->tm_mday;
+    for(int i=0;i<n;i++){
+        if(a[i].get_year()<=1850+ ltm->tm_year && a[i].get_month()<=1 + ltm->tm_mon && a[i].get_day()<=ltm->tm_mday){
+            a[i].display_all();
+            a[i].write_all(FILE);
+        }
+    }
+    //outfile<<"\n\n";
+    FILE.close();
+}
 int main(){
-    //STILL WORKING
     int n;
     ifstream infile; // Создаем потоковый объект infile ...
     // ... класса ifstream для чтения из файла
     infile.open("infile.txt"); // Открываем поток, связывая его с файлом
     infile>>n;
     employee *a= new employee[n];
-    for(int i=0;i<n;i++)
-        infile>>a[i].sname>>a[i].name>>a[i].patronymic>>a[i].date.day>>a[i].date.month
-           >>a[i].date.year>>a[i].division>>a[i].position;
+    for (int i = 0; i < n; i++)
+        a[i].read_from(infile);
     infile.close();
     int w=-1;
     while(w!=0){
-        cout<<"Enter task:\n1)Display names of employee of chosen division\n2)Display data of all employee beyond 50 years old\n0)Quit\n";
+        cout<<"Enter task:\n1)Display names of employee of chosen division\n2)Display data of all employee beyond 50 years old\n0-or any other)Quit\n";
         cin>>w;
         switch(w){
-            case 1:
-                fun1(a,n);
-                break;
-            case 2:
-                fun2(a,n);
-                break;
+            case 1:{
+                ofstream outfile;
+                outfile.open("outfile.txt");
+                fun1(a,n,outfile);
+                break;}
+            case 2:{
+                ofstream outfile;
+                outfile.open("outfile.txt");
+                fun2(a,n,outfile);
+                break;}
             default:
                 cout<<"Quited\n";
                 break;
--- OOP3.h
+++ OOP3.h
@@ -1,34 +1,40 @@
 #ifndef OOP_H
-#define OOP_H
 
-
-#include<iostream>
-#include<string>
-#include <ctime>
-#include<fstream>
-using std::string;
-using std::ifstream;
-
-
-struct bday{
-    int day;
-    int month;
-    int year;
-};
-typedef struct bday bday;
-
-
-class employee{
-    private:
-        string sname;
-        string name;
-        string patronymic;
-        bday date;
-        int division;
-        string position;
-    public:
+    #define OOP_H
+    #include<string>
+    #include <ctime>
+    #include<fstream>
+    using namespace std;
 
 
 
-};
+    struct bday{
+        int day;
+        int month;
+        int year;
+    };
+    typedef struct bday bday;
+
+
+    class employee{
+        private:
+            string sname;
+            string name;
+            string patronymic;
+            bday date;
+            int division;
+            string position;
+        public:
+            //void enter_struct(employee *a, int n, ifstream & FILE );
+            int get_division();
+            void display_all();
+            void write_all(ofstream &FILE);
+            void read_from(ifstream &FILE);
+            void display_name_n_position_only();
+            void write__name_n_position_only(ofstream &FILE);
+            int get_day();
+            int get_month();
+            int get_year();
+
+    };
 #endif // OOP_H
